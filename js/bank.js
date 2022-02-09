09bcef302e304d157641e657dc8c9518d64317c7
frsiam <siam.frs@gmail.com>
2022-02-10 00:18:12 +0600

    validation of email and password

--- js/bank.js
+++ js/bank.js
@@ -1,3 +1,16 @@
 function submit(){
-    alert();
+    // Get user Email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // Get user Password
+    const passwordField = document.getElementById('user-password')
+    const userPassword = passwordField.value;
+
+    // Validation
+    if(userEmail == 'son@gmail.com' && userPassword == '12345'){
+        console.log('thik ase')
+    }
+    else{
+        alert("thik nai.....valo kore mone kor")
+    }
 }
